031c1ac0a7a28b4601be0e2d334627dce1341c4e
Mateusz <m.jaroszewski17@gmail.com>
2022-06-26 18:03:58 +0200

    Initial Commit

---
+++ Pasta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pasta
+//
+//  Created by Mateusz Jaroszewski on 26/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pasta/PastaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastaApp.swift
+//  Pasta
+//
+//  Created by Mateusz Jaroszewski on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PastaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
